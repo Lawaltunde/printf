eab4db894a61c7b1399e08dcdab8e21fe3fdb078
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-23 15:09:35 -0700

    tunAugPrintf1.c

---
+++ is_puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putstrn - print out string
+ * @letter: input string.
+ * Return: val when string is passed in.
+ */
+
+int _putstrn(char *letter)
+{
+	unsigned int i, val = 0;
+
+	if(letter)
+	{
+		for (i = 0; letter[i] != '\0'; i++)
+		{
+			puchr(letter[i]);
+			val += 1;
+		}
+		val;
+	}
+	return (val);
+}

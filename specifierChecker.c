1e381c2ea859939dea16fbe84d9e624d8025c4ad
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-11 13:41:14 -0700

    specifierChecker.c

--- specifierChecker.c
+++ specifierChecker.c
@@ -3,18 +3,29 @@
 /**
   * specifierChecks - pointer to a function that checks for a
   * match between the input and the format specifier
-  * @s: pointer to char
+  * @s: char
+  * *pos: pointer to int
   * Return: pointer to function and an int value
   */
 
 
-int (*specifierChecks(const char *s))(va_list args, char *buf)
+int (*specifierChecks(const char s))(va_list arg_list, char *buffer, int *pos)
 {
 	format_sp fmptr[] = {
-		{"c", printChar},
-		{"s", printStr},
-		{"%", printPer},
-		{NULL, NULL}
+		{'c', printChar},
+		{'s', printStr},
+		{'%', printPer},
+		{'d', print_d_i},
+		{'i', print_d_i},
+		{'b', print_b},
+		{'r', print_r},
+		{'R', print_r_13},
+		{'p', print_p},
+		{'x', print_x},
+		{'X', print_X},
+		{'u', print_u},
+		{'o', print_o},
+		{'\0', NULL}
 	};
 	int i = 0;
 
@@ -24,7 +35,7 @@ int (*specifierChecks(const char *s))(va_list args, char *buf)
 		exit(1);
 		return (NULL);
 	}
-	while (fmptr[i].chr != NULL)
+	while (fmptr[i].chr != '\0')
 	{
 		if (s == fmptr[i].chr)
 		{

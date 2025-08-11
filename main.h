3e16519c8423b48bd277d9e9fdd362260405ffa9
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-11 13:41:02 -0700

    main.h

--- main.h
+++ main.h
@@ -11,25 +11,28 @@
   * function pointer and pointer to char
   * chr: char arg
   * @fptr: function to pointer
+  * @pos: buffer tracker
   */
 typedef struct formatter
 {
 	char chr;
-	int (*fptr)(va_list args, char *buffer);
+	int (*fptr)(va_list arg_list, char *buffer, int *pos);
 } format_sp;
 
 int _printf(const char *format, ...);
-int printChar(va_list args, char *buf);
-int printStr(va_list args, char *buf);
-int printPer(va_list __attribute__((__unused__))args, char *buf);
-int (*specifierChecks(const char s))(va_list args, char *buf);
-int print_d_i(va_list arg_list, char *buffer);
-void print_num(int n, char *buffer);
-int print_b(va_list arg_list, char *buffer);
-int print_r(va_list arg_list, char *buffer);
-int print_r_13(va_list arg_list, char *buffer);
-int print_x(va_list arg_list, char *buffer);
-int print_X(va_list arg_list, char *buffer);
-int print_u(va_list arg_list, char *buffer);
-int print_o(va_list arg_list, char *buffer);
+int printChar(va_list arg_list, char *buffer, int *pos);
+int printStr(va_list arg_list, char *buffer, int *pos);
+int printPer(va_list __attribute__((__unused__))arg_list, char *buffer, int *pos);
+int (*specifierChecks(const char s))(va_list arg_list, char *buffer, int *pos);
+int print_d_i(va_list arg_list, char *buffer, int *pos);
+void prnt(unsigned long n, unsigned int base, int uppercase, char *buffer, int *pos);
+int print_b(va_list arg_list, char *buffer, int *pos);
+int print_r(va_list arg_list, char *buffer, int *pos);
+int print_r_13(va_list arg_list, char *buffer, int *pos);
+int print_p(va_list arg_list, char *buffer, int *pos);
+int print_p(va_list arg_list, char *buffer, int *pos);
+int print_x(va_list arg_list, char *buffer, int *pos);
+int print_X(va_list arg_list, char *buffer, int *pos);
+int print_u(va_list arg_list, char *buffer, int *pos);
+int print_o(va_list arg_list, char *buffer, int *pos);
 #endif
